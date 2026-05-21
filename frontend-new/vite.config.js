46355817e0131a76b9bfcca3db8778f0f7abbd46
EHS System <ehs-system@github.com>
2026-05-21 13:22:12 +0800

    chore: 更新.gitignore排除临时文件，添加vite.config.js

---
+++ frontend-new/vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import createVitePlugins from './vite/plugins'
+
+export default defineConfig(({ mode, command }) => {
+  const isBuild = command === 'build'
+
+  return {
+    plugins: createVitePlugins({ VITE_BUILD_COMPRESS: 'gzip' }, isBuild),
+    resolve: {
+      alias: {
+        '@': '/src'
+      },
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    },
+    server: {
+      port: 5173,
+      strictPort: false,
+      proxy: {
+        '/dev-api': {
+          target: 'http://localhost:3001',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/dev-api/, '/api')
+        }
+      }
+    },
+    build: {
+      outDir: '../WWW',
+      emptyOutDir: true,
+      // 解决 Node.js heap out of memory 问题
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        external: ['jquery'],
+        output: {
+          manualChunks: {
+            'element-plus': ['element-plus'],
+            'echarts': ['echarts'],
+          }
+        }
+      }
+    }
+  }
+})
